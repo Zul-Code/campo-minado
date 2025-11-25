d220ce53206378a1c4645e5d50c81ea3bbc40dc2
G-Zanelatto <biel270000000008@gmail.com>
2025-11-25 17:25:00 -0300

    feat: Criação da função que cria o tabuleiro

--- jogo.c
+++ jogo.c
@@ -9,7 +9,20 @@ typedef struct {
     int vizinhos;
 } Celula;
 
+void inicializarTabuleiro(Celula tabuleiro[TAM][TAM]) {
+        for (int i = 0; i < TAM; i++) {
+            for (int j = 0; j < TAM; j++) {
+                tabuleiro[i][j].temBomba = 0;
+                tabuleiro[i][j].estaAberta = 0;
+                tabuleiro[i][j].vizinhos = 0;
+            }
+        }
+    }
+
 float jogarCampoMinado() {
+
+    Celula tabuleiro[TAM][TAM];
+    inicializarTabuleiro(tabuleiro);
 
     return 0.00;
 }
