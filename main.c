976b49653f21a5356a6cea53ea73c3264acb6b07
Thales Gregorio <137453849+GrowpGS@users.noreply.github.com>
2025-11-25 15:52:23 -0300

    feat: criacao do loop principal

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include "hud.h"
 
 int main() {
+    int escolha;
+
+    do {
+        mainHud();
+        scanf("%d", &escolha);
+    } while (escolha != 2);
+
     return 0;
 }
