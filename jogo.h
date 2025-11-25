c89b9ec27217f6ed3f8d9fdb745d30d8a534c472
Vitor Moreira <138052845+VitorMoreira44@users.noreply.github.com>
2025-11-25 20:11:05 -0300

    fix: disponibilizando as funcoes pros outros

--- jogo.h
+++ jogo.h
@@ -1,6 +1,10 @@
 #ifndef JOGO_H
 #define JOGO_H
+#include "celula.h"
+#define TAM 5
+#define BOMBAS 5
 
 float jogarCampoMinado();
+int contarVizinhos(Celula tabuleiro[TAM][TAM], int l, int c);
 
 #endif
