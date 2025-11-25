965862f8313bef189034806530d5d69f786df88a
G-Zanelatto <biel270000000008@gmail.com>
2025-11-25 17:22:29 -0300

    feat: Struct das células do tabuleiro

--- jogo.c
+++ jogo.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "jogo.h"
+#define TAM 5
+#define BOMBAS 5
+
+typedef struct {
+    int temBomba;
+    int estaAberta;
+    int vizinhos;
+} Celula;
 
 float jogarCampoMinado() {
+
     return 0.00;
 }
