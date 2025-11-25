970f9151c10bbe22a8a89afb3efb08f133267039
joaoallegre <jhallegre@gmail.com>
2025-11-25 20:34:00 -0300

    feat: colocando a funcao do jogo no main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "hud.h"
+#include "jogo.h"
 
 int main() {
     int escolha;
@@ -10,6 +11,10 @@ int main() {
 
         switch (escolha) {
             case 1:
+                float tempoGasto = jogarCampoMinado();
+                if (tempoGasto >= 0) {
+                    printf("Seu tempo: %.2f segundos.\n", tempoGasto);
+                }
                 break;
             case 2:
                 saindoHud();
