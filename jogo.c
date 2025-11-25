b33480bd1008d431b02ddcaf24a70374155e6e13
G-Zanelatto <biel270000000008@gmail.com>
2025-11-25 17:35:45 -0300

    feat: Criação da função que cria as bombas

--- jogo.c
+++ jogo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "jogo.h"
 #define TAM 5
 #define BOMBAS 5
@@ -16,13 +17,32 @@ void inicializarTabuleiro(Celula tabuleiro[TAM][TAM]) {
                 tabuleiro[i][j].estaAberta = 0;
                 tabuleiro[i][j].vizinhos = 0;
             }
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            tabuleiro[i][j].temBomba = 0;
+            tabuleiro[i][j].estaAberta = 0;
+            tabuleiro[i][j].vizinhos = 0;
         }
     }
+}
+
+void sortearBombas(Celula tabuleiro[TAM][TAM]) {
+    int colocadas = 0;
+    while (colocadas < BOMBAS) {
+        int r = rand() % TAM;
+        int c = rand() % TAM;
+        if (tabuleiro[r][c].temBomba == 0) {
+            tabuleiro[r][c].temBomba = 1;
+            colocadas++;
+        }
+    }
+}
 
 float jogarCampoMinado() {
 
     Celula tabuleiro[TAM][TAM];
     inicializarTabuleiro(tabuleiro);
+    sortearBombas(tabuleiro);
 
     return 0.00;
 }
