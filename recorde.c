22334624410419050539f2590770eb8fefa5fbc8
Miguel Rochia <migrochia10@gmail.com>
2025-11-26 18:18:20 +0000

    feat: criao dos arquivos para o recorde

---
+++ recorde.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "recorde.h"
+
+float carregarRecorde() {
+    return 0.00;
+}
+
+void salvarRecorde(float tempo) {
+}
---
+++ recorde.h
@@ -0,0 +1,7 @@
+#ifndef RECORDE_H
+#define RECORDE_H
+
+float carregarRecorde();
+void salvarRecorde(float tempo);
+
+#endif
