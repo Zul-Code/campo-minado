803c6a05509db8769090568721ea9b23a8f2e620
Miguel Rochia <migrochia10@gmail.com>
2025-11-26 18:19:30 +0000

    feat: criacao da funcao de salvar recorde

--- recorde.c
+++ recorde.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include "recorde.h"
+#define ARQUIVO_NOME "recorde.txt"
 
 float carregarRecorde() {
     return 0.00;
 }
 
 void salvarRecorde(float tempo) {
+    float recordeAtual = carregarRecorde();
+
+    if (tempo < recordeAtual) {
+        FILE *arquivo = fopen(ARQUIVO_NOME, "w");
+        if (arquivo != NULL) {
+            fprintf(arquivo, "%.2f", tempo);
+            fclose(arquivo);
+            printf("\n[!] Novo recorde salvo: %.2f segundos!\n", tempo);
+        } else {
+            printf("\n[!] Erro ao salvar o recorde.\n");
+        }
+    }
 }
